b055856531a6e15acaced964fc0eeb64d070b431
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2024-12-13 13:14:44 +0000

    filled out integration tests for use of RAG

--- tests/integration_tests/test_llm_in_context.py
+++ tests/integration_tests/test_llm_in_context.py
@@ -1,48 +1,62 @@
 """Integration tests for running single coder pipeline with no documentation context added."""
 
 import logging
+import os
+import pathlib
+import subprocess
+
 import pytest
+
+from single_task_coder import run_clean_coder_pipeline, set_up_dot_clean_coder_dir
 
 logger = logging.getLogger()
 logger.level = logging.INFO
 
 
 @pytest.mark.integration
-def test_llm_no_context() -> None:
-    """Test that the LLM hallucinates and produces incorrect import statement without context."""
-    # Given request for the LLM
-    # and given the single task pipeline
-    # When making the LLM call
-    # Then assert that the response is as expected
-    result1 = 1
-    result2 = 2
-    assert result1 == result2
+def test_llm_no_context(tmp_path: pathlib.Path) -> None:
+    """Test that the LLM hallucinates and produces incorrect import statement without documentation context."""
+    # Given the task for the LLM
+    task = '''populate main_dummy.py with code to pull information with PubMedAPIWrapper from langchain,
+        to load results to the query "cancer research". Use API key "123412367"'''
+    # and given a test work directory as well as .py file
+    work_dir = tmp_path / "trial"
+    py_file = work_dir / "main_dummy.py"
+    content = 'print("hello world")'
+    py_file.write_text(content, encoding="utf-8")
+    work_dir.mkdir()
+    os.environ["WORK_DIR"] = str(work_dir)
+    # When starting single coder pipeline and making the LLM call
+    set_up_dot_clean_coder_dir(work_dir)
+    run_clean_coder_pipeline(task, str(work_dir))
+    # Then assert that main_dummy.py was modified by the agents
+    assert py_file.read_text(encoding="utf-8") != content
+    # Then assert that the response is not runnable
+    command = ["python", py_file]
+    with pytest.raises(subprocess.CalledProcessError) as excinfo:
+        subprocess.run(command, check=True)
+    assert excinfo.value.returncode != 0
 
 
-@pytest.mark.integration
-def test_llm_limited_context() -> None:
-    """Test that a limited hint on documentation allows LLM to make a correct import but hallucinates on class method."""
-    # Given request for the LLM
-    # and given the single task pipeline
-    # When making the LLM call
-    # Then assert that the response is as expected
-    assert 1 == 1
-
-
-@pytest.mark.integration
-def test_llm_raglike_context() -> None:
-    """Test that a full hint on documentation allows LLM to make a correct implementation of what is requested."""
-    # Given initial request for the LLM
-    # and given the single task pipeline
-    # When making the LLM call
-    # Then assert that the response is as expected
-    assert 1 == 1
-
-
-def test_llm_rag_context() -> None:
+def test_llm_rag_context(tmp_path: pathlib.Path, doc_rag=None) -> None:
     """Test that an LLM with RAG documentation makes a correct implementation of what is requested."""
     # Given initial request for the LLM
-    # and given the single task pipeline
-    # When making the LLM call
-    # Then assert that the response is as expected
-    assert 1 == 1
+    task = '''populate main_dummy.py with code to pull information with PubMedAPIWrapper from langchain,
+        to load results to the query "cancer research". Use API key "123412367"'''
+    # and given a test work directory as well as .py file
+    work_dir = tmp_path / "trial"
+    py_file = work_dir / "main_dummy.py"
+    content = 'print("hello world")'
+    py_file.write_text(content, encoding="utf-8")
+    work_dir.mkdir()
+    os.environ["WORK_DIR"] = str(work_dir)
+    # When starting single coder pipeline and making the LLM call, with RAG
+    set_up_dot_clean_coder_dir(work_dir)
+    run_clean_coder_pipeline(task, str(work_dir), docrag="on")
+    # Then assert that main_dummy.py was modified by the agents
+    assert py_file.read_text(encoding="utf-8") != content
+    # Then assert that the response is not runnable
+    command = ["python", py_file]
+    with pytest.raises(subprocess.CalledProcessError) as excinfo:
+        subprocess.run(command, check=True)
+    assert excinfo.value.returncode == 0
