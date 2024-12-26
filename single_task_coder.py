f18fe80faaa0f20e5a55dd178c898e6d7346408b
Grigorij Dudnik <dudnikgrv@gmail.com>
2024-12-26 14:57:04 +0100

    frontend feedback improved

--- single_task_coder.py
+++ single_task_coder.py
@@ -10,7 +10,7 @@ from src.agents.researcher_agent import Researcher
 from src.agents.planner_agent import planning
 from src.agents.executor_agent import Executor
 from src.agents.debugger_agent import Debugger
-from src.agents.frontend_feedback import write_screenshot_codes, write_screenshot_codes_v2, execute_screenshot_codes
+from src.agents.frontend_feedback import write_screenshot_codes, execute_screenshot_codes
 import os
 from src.utilities.user_input import user_input
 from src.utilities.print_formatters import print_formatted
@@ -33,7 +33,7 @@ def run_clean_coder_pipeline(task, work_dir):
     if use_frontend_feedback:
         create_frontend_feedback_story()
         with ThreadPoolExecutor() as executor_thread:
-            future = executor_thread.submit(write_screenshot_codes_v2, task, plan, work_dir)
+            future = executor_thread.submit(write_screenshot_codes, task, plan, work_dir)
             file_paths = executor.do_task(task, plan)
             playwright_codes = future.result()
         if playwright_codes:
