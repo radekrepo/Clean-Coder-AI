45225d28ea324398184d1f470e84edca9f37570d
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-01-09 08:56:30 +0000

    checkpoint

--- src/agents/rag_researcher.py
+++ src/agents/rag_researcher.py
@@ -1,66 +1,67 @@
+if not os.environ.get("COHERE_API_KEY"):
+  os.environ["COHERE_API_KEY"] = getpass.getpass("Enter API key for Cohere: ")
 
-import getpass
-import os
+from langchain_cohere import ChatCohere, CohereEmbeddings
 
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
-
-
-
-response = graph.invoke({"question": "What is Task Decomposition?"})
-print(response["answer"])
-
-# from dotenv import load_dotenv, find_dotenv
-# from langchain.agents import initialize_agent
-# from langchain_community.llms import OpenAI
-# from langchain.chains import RetrievalQA
-# from langchain_community.document_loaders import TextLoader
-# from langchain.memory import ConversationBufferMemory
-# from langchain_openai.embeddings import OpenAIEmbeddings
-# from langchain_community.vectorstores import FAISS
-# from pathlib import Path
-# import os
-
-# load_dotenv(find_dotenv())
-# mistral_api_key = os.getenv("MISTRAL_API_KEY")
-# anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
-# openai_api_key = os.getenv("OPENAI_API_KEY")
-# work_dir = os.getenv("WORK_DIR")
-# if not work_dir:
-#     raise Exception("work_dir environment variable is not set!")
-
-# # Load your documents
-# loader = TextLoader(str(Path(work_dir).joinpath(".clean_coder").joinpath("rag_documents.txt")))
-# documents = loader.load()
-
-# # Create embeddings 
-# embeddings = OpenAIEmbeddings()
-
-# # Create a vectorstore 
-# vectorstore = FAISS.from_documents(documents, embeddings)
-
-# # Create a retriever
-# retriever = vectorstore.as_retriever(n_results=2)
-
-# # Create a RetrievalQA chain
-# qa_chain = RetrievalQA.from_chain_type(
-#     llm=OpenAI(),
-#     chain_type="stuff",
-#     retriever=retriever,
-#     verbose=True
-# )
-
-# # Initialize the agent
-# agent = initialize_agent(
-#     llm=OpenAI(),
-#     # agent=agent_tools,
-#     tools=[qa_chain],
-#     memory=ConversationBufferMemory(),
-#     verbose=True
-# )
+llm = ChatCohere(model="command-r-plus")
+embeddings = CohereEmbeddings(model="embed-english-v3.0")
 
 
-# if __name__ == "__main__":
-#     task = "What is the capital of France?"
-#     # Now you can interact with the agent
-#     agent.run(task)
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+
+# Define prompt for question-answering
+prompt = hub.pull("rlm/rag-prompt")
+
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+
+if __name__ == "__main__":
+    response = graph.invoke({"question": "What is Task Decomposition?"})
+    print(response["answer"])
