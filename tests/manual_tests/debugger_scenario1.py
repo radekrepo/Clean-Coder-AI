a47d0be7aca5045aa0c70590ad2482a0462276f2
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-01-16 09:16:16 +0000

    debugger scenario fully operational after updates

--- tests/manual_tests/debugger_scenario1.py
+++ tests/manual_tests/debugger_scenario1.py
@@ -1,8 +1,8 @@
 """Debugger scenario 1."""
-import pathlib
 import sys
+from pathlib import Path
 
-repo_directory = pathlib.Path(__file__).parents[2].resolve()
+repo_directory = Path(__file__).parents[2].resolve()
 sys.path.append(str(repo_directory))
 from dotenv import find_dotenv, load_dotenv
 
@@ -11,12 +11,13 @@ from tests.manual_tests.utils_for_tests import cleanup_work_dir, get_filenames_i
 
 load_dotenv(find_dotenv())
 
-folder_with_project_files = str(repo_directory.joinpath("tests", "manual_tests", "projects_files", "debugger_scenario_1_files"))
-setup_work_dir(folder_with_project_files)
-files = get_filenames_in_folder(folder_with_project_files)
+folder_with_project_files = repo_directory.joinpath("tests", "manual_tests", "projects_files", "debugger_scenario_1_files")
+tmp_folder =  Path(__file__).parent.resolve().joinpath("sandbox_work_dir")
+setup_work_dir(test_files_dir=folder_with_project_files, manual_tests_folder=tmp_folder)
+files = get_filenames_in_folder(manual_tests_folder=tmp_folder)
 human_feedback = "Human feedback: Remove some code from the styles. It's too much of code now, let's optimize it."
 
-debugger = Debugger(files, "sandbox_work_dir", human_feedback, [])
+debugger = Debugger(files, str(tmp_folder), human_feedback, [])
 
 task = "Make form wider, with green background. Improve styling."
 plan = """To make the form wider and change its background color to green, we need to modify the CSS file `profileEditStyles.css`. Here are the changes needed:
@@ -43,4 +44,4 @@ These changes will ensure that the form is wider with a green background, improv
 """
 
 debugger.do_task(task, plan)
-cleanup_work_dir()
+cleanup_work_dir(manual_tests_folder=tmp_folder)
