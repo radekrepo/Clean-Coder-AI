f1b9b9b0108f8406db166796245c603b211f49fe
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-01-18 07:31:13 +0000

    update paths to files. Planner scenarios both working

--- tests/manual_tests/planer_scenario_1.py
+++ tests/manual_tests/planer_scenario_1.py
@@ -11,7 +11,7 @@ from tests.manual_tests.utils_for_tests import cleanup_work_dir, get_filenames_i
 
 load_dotenv(find_dotenv())
 
-folder_with_project_files = repo_directory.joinpath("tests", "manual_tests", "projects_files", "executor_scenario_1_files")
+folder_with_project_files = repo_directory.joinpath("tests", "manual_tests", "projects_files", "planner_scenario_1_files")
 tmp_folder =  pathlib.Path(__file__).parent.resolve().joinpath("sandbox_work_dir")
 setup_work_dir(manual_tests_folder=tmp_folder, test_files_dir=folder_with_project_files)
 
