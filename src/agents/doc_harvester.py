a301320dca722e085893a48976782babeb941e6e
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-01-28 14:34:09 +0000

    documentation collection step ready

--- src/agents/doc_harvester.py
+++ src/agents/doc_harvester.py
@@ -1,14 +1,28 @@
 """Documentation harvester pulls relevant docs for the task set by the user of the pipeline."""
 
+import importlib
 import os
-from typing import Union
-import asyncio
+import subprocess
+import sys
+from pathlib import Path
+
 from crawl4ai import AsyncWebCrawler
+from crawl4ai.models import CrawlResult
 from dotenv import find_dotenv, load_dotenv
+
+from src.utilities.exceptions import ModuleImportedButNotLocatedError
 
 load_dotenv(find_dotenv())
 
 # playwright install --with-deps chromium
+
+async def pull_webpage(url: str) -> CrawlResult:
+    """Pulls URL information."""
+    async with AsyncWebCrawler() as crawler:
+        return await crawler.arun(
+            url=url,
+        )
+
 
 class DocHarvester:
     """
@@ -36,27 +50,36 @@ class DocHarvester:
         """Initial information to help harvest documentation."""
         self.work_dir = os.getenv("WORK_DIR")
     def identify_libraries(self, task: str) -> list[str]:
-        """Library names relevant for user's task."""
+        """Library names relevant for user's task. An LLM task."""
         libraries = []
         return libraries
-    def url_for_library(self, library: str) -> str:
-        url = ""
-        return url
-    def identify_documentation(self, task: str) -> list[str]:
-        """Find web addresses of software packages useful for the task."""
-        libraries = self.identify_libraries(task=task)
-        return [self.url_for_library(library) for library in libraries]
-    async def pull_webpage(self, url: str):
-        """Pulls URL information."""
-        scraped = []
-        async with AsyncWebCrawler() as crawler:
-            result = await crawler.arun(
-                url=url,
-            )
-            scraped.append(result)
-        return scraped
-    def find_documentation(self, task: str) -> None | list[str]:
+
+    def locate_module_files(self, lib: str) -> Path:
+        """Identify locations where module scripts are stored."""
+        imported = importlib.import_module(lib)
+        if imported.__file__:
+            return Path(imported.__file__).parent
+        msg = f"'{lib}' imported but not found."
+        raise ModuleImportedButNotLocatedError(msg)
+
+    def identify_documentation(self, libraries: list[str]) -> dict[str, str]:
+        """Find files of software packages useful for the task, including docstrings."""
+        installed = {pkg.metadata["name"] for pkg in importlib.metadata.distributions()}
+        missing = set(libraries) - installed
+        if missing:
+            # install lib
+            python = sys.executable
+            subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+        lib_documentation = {}
+        for lib in libraries:
+            lib_documentation[lib] = self.locate_module_files(lib=lib)
+        return lib_documentation
+
+    def rag_ready(self, rag_input: dict[str, str]) -> None:
+        """Prepare RAG-ready data from scripts in directories indicated in the input."""
+
+    def rag_documentation(self, task: str) -> None | list[str]:
         """Returns documentation relevant for the task set by human user, a list of files."""
-        documentation_urls = self.identify_documentation(task=task)
-        rag_input = [self.pull_webpage(url=webpage) for webpage in documentation_urls]
-        return None
+        libraries = self.identify_libraries(task=task)
+        rag_input = self.identify_documentation(libraries=libraries)
+        self.rag_ready(rag_input=rag_input)
--- src/utilities/exceptions.py
+++ src/utilities/exceptions.py
@@ -2,3 +2,6 @@
 
 class MissingEnvironmentVariableError(Exception):
     """Enviromental variable missing."""
+
+class ModuleImportedButNotLocatedError(Exception):
+    """A module was imported but the location of its source files is unknown."""
