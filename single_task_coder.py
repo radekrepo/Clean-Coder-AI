f3bbf86b527d1e2d4d747e55bdfc6e6ee73fe436
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-01-28 08:29:45 +0000

    adapt single task coder to DocHarvester. Minor good practice corrections

--- single_task_coder.py
+++ single_task_coder.py
@@ -7,12 +7,13 @@ if not find_dotenv():
     set_up_env_coder_pipeline()
 
 from src.agents.researcher_agent import Researcher
-from src.agents.doc_harvester import Doc_harvester
+from src.agents.doc_harvester import DocHarvester
 from src.agents.planner_agent import planning
 from src.agents.executor_agent import Executor
 from src.agents.debugger_agent import Debugger
 from src.agents.frontend_feedback import write_screenshot_codes
 import os
+from src.utilities.exceptions import MissingEnvironmentVariableError
 from src.utilities.user_input import user_input
 from src.utilities.start_project_functions import set_up_dot_clean_coder_dir
 from src.utilities.util_functions import create_frontend_feedback_story
@@ -22,13 +23,14 @@ from concurrent.futures import ThreadPoolExecutor
 use_frontend_feedback = bool(os.getenv("FRONTEND_URL"))
 
 
-def run_clean_coder_pipeline(task: str, work_dir: str, doc_harvest: bool = False):
+def run_clean_coder_pipeline(task: str, work_dir: str, doc_harvest: bool = False) -> None:
+    """Single run of clean code pipeline to address a task., from Researcher to Debugger."""
     researcher = Researcher(work_dir)
     file_paths, image_paths = researcher.research_task(task)
     documentation = None
     if doc_harvest:
-        harvester = Doc_harvester()
-        documentation = harvester.find_documentation(task, work_dir)
+        harvester = DocHarvester()
+        documentation = harvester.find_documentation(task)
 
     plan = planning(task, file_paths, image_paths, work_dir, documentation=documentation)
 
@@ -45,7 +47,7 @@ def run_clean_coder_pipeline(task: str, work_dir: str, doc_harvest: bool = False
         file_paths = executor.do_task(task, plan)
 
     human_message = user_input("Please test app and provide commentary if debugging/additional refinement is needed. ")
-    if human_message in ['o', 'ok']:
+    if human_message in ["o", "ok"]:
         return
     debugger = Debugger(
         file_paths, work_dir, human_message,image_paths,  playwright_codes)
@@ -57,5 +59,6 @@ if __name__ == "__main__":
     set_up_dot_clean_coder_dir(work_dir)
     task = user_input("Provide task to be executed. ")
     if not work_dir:
-        raise Exception("WORK_DIR variable not provided. Please add WORK_DIR to .env file")
+        msg = "WORK_DIR variable not provided. Please add WORK_DIR to .env file"
+        raise MissingEnvironmentVariableError(msg)
     run_clean_coder_pipeline(task, work_dir)
