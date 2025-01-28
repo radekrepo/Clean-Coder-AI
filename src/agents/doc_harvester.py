d5bf5a79887872e3f578c95703a64d826041d0e0
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-01-28 08:30:31 +0000

    function frame up to vectorization

--- src/agents/doc_harvester.py
+++ src/agents/doc_harvester.py
@@ -1,11 +1,62 @@
-"""Documentation harvester pulls relevant documentation for the task by user of the pipeline."""
+"""Documentation harvester pulls relevant docs for the task set by the user of the pipeline."""
 
+import os
 from typing import Union
+import asyncio
+from crawl4ai import AsyncWebCrawler
+from dotenv import find_dotenv, load_dotenv
 
-class Doc_harvester:
+load_dotenv(find_dotenv())
+
+# playwright install --with-deps chromium
+
+class DocHarvester:
+    """
+    Agent for collecting documentation relevant to user's task. Requires internet access.
+
+    More description.
+
+    Attributes
+    ----------
+    work_dir: str
+        Location of the project that Clean Coder pipeline operates on.
+
+    Methods
+    -------
+        find_documentation(task: str)
+
+    Examples
+    --------
+        dh = DocHarvester()
+        task = "prepare a scraper of a website"
+        dh.find_documentation(task=task)
+    """
+
     def __init__(self) -> None:
-        """Initial information to help harvest documentation from the internet."""
-        pass
-    def find_documentation(self, task: str, work_dir: str) -> Union[None, list[str]]:
-        """Returns documentation relevant for the task set by human user."""
+        """Initial information to help harvest documentation."""
+        self.work_dir = os.getenv("WORK_DIR")
+    def identify_libraries(self, task: str) -> list[str]:
+        """Library names relevant for user's task."""
+        libraries = []
+        return libraries
+    def url_for_library(self, library: str) -> str:
+        url = ""
+        return url
+    def identify_documentation(self, task: str) -> list[str]:
+        """Find web addresses of software packages useful for the task."""
+        libraries = self.identify_libraries(task=task)
+        return [self.url_for_library(library) for library in libraries]
+    async def pull_webpage(self, url: str):
+        """Pulls URL information."""
+        scraped = []
+        async with AsyncWebCrawler() as crawler:
+            result = await crawler.arun(
+                url=url,
+            )
+            scraped.append(result)
+        return scraped
+    def find_documentation(self, task: str) -> None | list[str]:
+        """Returns documentation relevant for the task set by human user, a list of files."""
+        documentation_urls = self.identify_documentation(task=task)
+        rag_input = [self.pull_webpage(url=webpage) for webpage in documentation_urls]
         return None
