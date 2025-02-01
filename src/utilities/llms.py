eb10dd711d16f447a29d81c90486445684716221
Grigorij Dudnik <dudnikgrv@gmail.com>
2025-02-01 11:16:42 +0100

    annotateted finish researcher

--- src/utilities/llms.py
+++ src/utilities/llms.py
@@ -76,7 +76,7 @@ def init_llms_mini(tools=None, run_name="Clean Coder", temp=0):
 def init_llms_planer(tools=None, run_name="Clean Coder", temp=0.2):
     llms = []
     if os.getenv("OPENAI_API_KEY"):
-        llms.append(ChatOpenAI(model="o1", temperature=1, timeout=60))
+        llms.append(ChatOpenAI(model="o3-mini", temperature=1, timeout=60))
     if os.getenv("OPENROUTER_API_KEY"):
         llms.append(llm_open_router("openai/gpt-4o"))
     if os.getenv("OPENAI_API_KEY"):
