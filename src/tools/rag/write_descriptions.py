689beaa4c7c6c0ac9ae49f32fba71e30aeabd696
Radek Kowalski <r.kowalski.pyzdry@gmail.com>
2025-02-11 09:52:09 +0000

    black reformat

--- src/tools/rag/write_descriptions.py
+++ src/tools/rag/write_descriptions.py
@@ -1,4 +1,5 @@
 """Functions to create an index of files for RAG."""
+
 import logging
 import os
 import sys
@@ -19,6 +20,7 @@ from src.utilities.llms import init_llms_mini
 ## Configure the logging level
 logging.basicConfig(level=logging.INFO)
 
+
 def relevant_extension(file_path: Path, file_extension_constraint: set[str]) -> bool:
     """Checker for whether file extension indicates a script."""
     # List of common code file extensions
@@ -38,35 +40,42 @@ def evaluate_file(root: str, file: str, file_extension_constraint: set[str] | No
     file_path = Path(root).joinpath(file)
     if len(ignore.intersection(file_path.parts)) > 0:
         return None
-    if file_extension_constraint and relevant_extension(file_path, file_extension_constraint=file_extension_constraint):
-            return file_path
+    if file_extension_constraint and relevant_extension(
+        file_path, file_extension_constraint=file_extension_constraint
+    ):
+        return file_path
     return None
 
 
 def files_in_directory(
-                       directories_with_files_to_describe: list[str | Path],
-                       file_extension_constraint: set[str] | None,
-                       ignore: set[str],
-                       ) -> list[Path]:
+    directories_with_files_to_describe: list[str | Path],
+    file_extension_constraint: set[str] | None,
+    ignore: set[str],
+) -> list[Path]:
     """Fetch paths of files in directory."""
     files_to_describe = []
     for directory in directories_with_files_to_describe:
         directory_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-        tmp = [evaluate_file(root=str(directory),
-                             file=file,
-                             file_extension_constraint=file_extension_constraint,
-                             ignore=ignore,
-                             ) 
-                             for file in directory_files
-                ]
+        tmp = [
+            evaluate_file(
+                root=str(directory),
+                file=file,
+                file_extension_constraint=file_extension_constraint,
+                ignore=ignore,
+            )
+            for file in directory_files
+        ]
         files_to_describe.extend(tmp)
         for root, _, files in os.walk(directory):
-            tmp = [evaluate_file(root=root,
-                                 file=file,
-                                 file_extension_constraint=file_extension_constraint,
-                                 ignore=ignore,
-                                 ) for file in files
-                ]
+            tmp = [
+                evaluate_file(
+                    root=root,
+                    file=file,
+                    file_extension_constraint=file_extension_constraint,
+                    ignore=ignore,
+                )
+                for file in files
+            ]
             files_to_describe.extend(tmp)
     return files_to_describe
 
@@ -79,31 +88,37 @@ def save_file_description(file_path: Path, work_dir: str, description: str, file
         out_file.write(description)
 
 
-def output_descriptions(files_to_describe: list[Path], chain: RunnableSequence, file_description_dir: str, work_dir: str) -> None:
+def output_descriptions(
+    files_to_describe: list[Path], chain: RunnableSequence, file_description_dir: str, work_dir: str
+) -> None:
     """Generate & output file descriptions to designated directory in WORK_DIR."""
     # iterate over all files, take 8 files at once
     batch_size = 8
     for i in range(0, len(files_to_describe), batch_size):
-        files_iteration = [f for f in files_to_describe[i:i + batch_size] if f is not None]
+        files_iteration = [f for f in files_to_describe[i : i + batch_size] if f is not None]
         descriptions = chain.batch([get_content(file_path) for file_path in files_iteration])
         logging.debug(descriptions)
-        [save_file_description(file_path=file_path,
-                               work_dir=work_dir,
-                               description=description,
-                               file_description_dir=file_description_dir,
-                               ) for file_path,
-                               description in zip(files_iteration, descriptions, strict=True)]
+        [
+            save_file_description(
+                file_path=file_path,
+                work_dir=work_dir,
+                description=description,
+                file_description_dir=file_description_dir,
+            )
+            for file_path, description in zip(files_iteration, descriptions, strict=True)
+        ]
 
 
-def produce_descriptions(directories_with_files_to_describe: list[str | Path],
-                       file_description_dir: str,
-                       work_dir: str,
-                       ignore: set[str],
-                       file_extension_constraint: set[str] | None = None,
-                       ) -> None:
+def produce_descriptions(
+    directories_with_files_to_describe: list[str | Path],
+    file_description_dir: str,
+    work_dir: str,
+    ignore: set[str],
+    file_extension_constraint: set[str] | None = None,
+) -> None:
     """
     Produce short descriptions of files. Store the descriptions in .clean_coder folder in WORK_DIR.
-    
+
     Inputs:
         directories_with_files_to_describe: directories from which files are to be described.
         file_description_dir: directory where generated file descriptions are to be saved to.
@@ -129,13 +144,14 @@ def produce_descriptions(directories_with_files_to_describe: list[str | Path],
                         ignore=ignore,
                         )
     """
-    files_to_describe = files_in_directory(directories_with_files_to_describe=directories_with_files_to_describe,
-                                           file_extension_constraint=file_extension_constraint,
-                                           ignore=ignore,
-                                           )
+    files_to_describe = files_in_directory(
+        directories_with_files_to_describe=directories_with_files_to_describe,
+        file_extension_constraint=file_extension_constraint,
+        ignore=ignore,
+    )
 
     prompt = ChatPromptTemplate.from_template(
-    """Describe the following code in 4 sentences or less, focusing only on important information from integration point of view.
+        """Describe the following code in 4 sentences or less, focusing only on important information from integration point of view.
     Write what file is responsible for.\n\n'''\n{code}'''
     """,
     )
@@ -144,7 +160,9 @@ def produce_descriptions(directories_with_files_to_describe: list[str | Path],
     llm = llms[0]
     chain = prompt | llm | StrOutputParser()
     Path(file_description_dir).mkdir(parents=True, exist_ok=True)
-    output_descriptions(files_to_describe=files_to_describe, work_dir=work_dir, chain=chain, file_description_dir=file_description_dir)
+    output_descriptions(
+        files_to_describe=files_to_describe, work_dir=work_dir, chain=chain, file_description_dir=file_description_dir
+    )
 
 
 def upload_to_collection(collection: chromadb.PersistentClient, file_description_dir: str) -> None:
@@ -162,16 +180,21 @@ def upload_to_collection(collection: chromadb.PersistentClient, file_description
             )
 
 
-def upload_descriptions_to_vdb(chroma_collection_name: str, work_dir: str, file_description_dir: str, vdb_location: str = ".clean_coder/chroma_base") -> None:
+def upload_descriptions_to_vdb(
+    chroma_collection_name: str,
+    work_dir: str,
+    file_description_dir: str,
+    vdb_location: str = ".clean_coder/chroma_base",
+) -> None:
     """
     Upload file descriptions to chroma database.
-    
+
     Inputs:
         chroma_collection_name: name of the collection within Chroma vector database to save file descriptions in.
         file_description_dir: directory where generated file descriptions are available.
         work_dir: project directory worked on with Clean Coder.
         vdb_location: (optional) location for storing the vector database.
-    
+
     Example:
         work_dir = os.getenv("WORK_DIR") # provide your own directory of choice if WORK_DIR is not set.
         if not work_dir:
@@ -202,7 +225,7 @@ def upload_descriptions_to_vdb(chroma_collection_name: str, work_dir: str, file_
 
 
 if __name__ == "__main__":
-    #provide optionally which subfolders needs to be checked, if you don't want to describe all project folder
+    # provide optionally which subfolders needs to be checked, if you don't want to describe all project folder
     # load environment
     load_dotenv(find_dotenv())
     work_dir = os.getenv("WORK_DIR")
@@ -211,15 +234,39 @@ if __name__ == "__main__":
         raise MissingEnvironmentVariableError(msg)
     file_description_dir = join_paths(work_dir, ".clean_coder/workdir_file_descriptions")
     file_extension_constraint = {
-        ".js", ".jsx", ".ts", ".tsx", ".vue", ".py", ".rb", ".php", ".java", ".c", ".cpp", ".cs", ".go", ".swift",
-        ".kt", ".rs", ".htm",".html", ".css", ".scss", ".sass", ".less", ".prompt",
+        ".js",
+        ".jsx",
+        ".ts",
+        ".tsx",
+        ".vue",
+        ".py",
+        ".rb",
+        ".php",
+        ".java",
+        ".c",
+        ".cpp",
+        ".cs",
+        ".go",
+        ".swift",
+        ".kt",
+        ".rs",
+        ".htm",
+        ".html",
+        ".css",
+        ".scss",
+        ".sass",
+        ".less",
+        ".prompt",
     }
     ignore = {".clean_coder", ".coderrules"}
-    produce_descriptions(directories_with_files_to_describe=[work_dir],
-                       file_description_dir=file_description_dir,
-                       work_dir=work_dir,
-                       file_extension_constraint=file_extension_constraint,
-                       ignore=ignore,
-                       )
+    produce_descriptions(
+        directories_with_files_to_describe=[work_dir],
+        file_description_dir=file_description_dir,
+        work_dir=work_dir,
+        file_extension_constraint=file_extension_constraint,
+        ignore=ignore,
+    )
     chroma_collection_name = f"clean_coder_{Path(work_dir).name}_file_descriptions"
-    upload_descriptions_to_vdb(chroma_collection_name=chroma_collection_name, work_dir=work_dir, file_description_dir=file_description_dir)
+    upload_descriptions_to_vdb(
+        chroma_collection_name=chroma_collection_name, work_dir=work_dir, file_description_dir=file_description_dir
+    )
